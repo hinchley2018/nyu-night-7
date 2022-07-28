28c7498fae5e0a28f88d42354dcb6fd41798fa9e
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-07-27 20:14:47 -0500

    add click listeners and buttons

--- index.js
+++ index.js
@@ -66,25 +66,66 @@ for (let index = 0; index < saleItems.length; index++) {
 
 //add our items to a ul
 //for now add these using js, later the user will
-let pastaLi = document.createElement("li")
-pastaLi.textContent = "Pasta - 1.29"
-pastaLi.setAttribute("class","sale")
-//add the li to the ul group for the cart
-cartList.append(pastaLi)
+// let pastaLi = document.createElement("li")
+// pastaLi.textContent = "Pasta - 1.29"
+// pastaLi.setAttribute("class","sale")
+// //add the li to the ul group for the cart
+// cartList.append(pastaLi)
 
-let potatoesLi = document.createElement("li")
-potatoesLi.textContent = "Potatoes - .30"
-potatoesLi.setAttribute("class","sale")
-//add the li to the ul group for the cart
-cartList.append(potatoesLi)
+// let potatoesLi = document.createElement("li")
+// potatoesLi.textContent = "Potatoes - .30"
+// potatoesLi.setAttribute("class","sale")
+// //add the li to the ul group for the cart
+// cartList.append(potatoesLi)
 
 //remove is a little tricky, make sure to call .remove() only on the tag you want to remove
 //for example if you call document.body.remove() it will remove the entire body
 //now remove the potatoes from cart
-potatoesLi.remove()
+// potatoesLi.remove()
 
 //images
 let catImg = document.createElement("img")
 catImg.setAttribute("src","https://placekitten.com/200/200")
 catImg.setAttribute("alt", "cat image")
 document.body.append(catImg)
+
+//when this function / recipe is used add item to cart
+function addItemToCart(item, price){
+    console.log("added item to cart", item, price)
+    let itemLi = document.createElement("li")
+    itemLi.textContent = `${item} - $${price}`
+    let removeButton = document.createElement("button")
+    removeButton.textContent = "Remove"
+    //not calling it just deifning the recipe
+    removeButton.addEventListener("click", function(){
+        //have a ref, we can just remove it
+        itemLi.remove()
+    })
+    itemLi.append(removeButton)
+    //add the li to the ul group for the cart
+    cartList.append(itemLi)
+}
+
+//get a ref to the stake button
+let steakButton = document.querySelector("#steak-button")
+//register a click listern, tell it what function to run,
+// the function can be inline like below or a predefined function
+steakButton.addEventListener("click", function(event){
+    console.log(event.target)
+    addItemToCart("Steak", 10.99)
+})
+
+
+let tomatoesButton = document.querySelector("#tomatoes-button")
+tomatoesButton.addEventListener("click", function(event){
+    addItemToCart("Tomatoes", 1.29)
+})
+
+let potatoesButton = document.querySelector("#potatoes-button")
+potatoesButton.addEventListener("click", function(event){
+    addItemToCart("Potatoes", .30)
+})
+let pastaButton = document.querySelector("#pasta-button")
+pastaButton.addEventListener("click", function(event){
+    addItemToCart("Pasta", 1.29)
+})
